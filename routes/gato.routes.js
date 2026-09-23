b765064505bb5b3ae50f2731afc586f62c04f43e
magdielu <magdiurbina21@gmail.com>
2026-09-22 18:01:41 -0600

    Commit backend v4

--- routes/gato.routes.js
+++ routes/gato.routes.js
@@ -1,6 +1,10 @@
 import express from "express";
 import multer from "multer";
-import { registrarGato, obtenerGatos, actualizarGato } from "../controllers/gato.controller.js";
+import { 
+  registrarGato, 
+  obtenerGatos, 
+  actualizarGato 
+} from "../controllers/gato.controller.js";
 
 const router = express.Router();
 
@@ -11,18 +15,13 @@ const upload = multer({
   },
 });
 
-router.post(
-  "/gato",
-  upload.single("imagen"),
-  registrarGato
-);
+// Crear un nuevo gato (POST /gato)
+router.post("/gato", upload.single("imagen"), registrarGato);
 
-router.put(
-  "/gato",
-  upload.single("imagen"),
-  actualizarGato
-);
-
-
+// Obtener todos los gatos (GET /gatos)
 router.get("/gatos", obtenerGatos);
+
+// Actualizar un gato por ID (PUT /gato/:id)  <--- ¡AQUÍ ESTABA EL ERROR!
+router.put("/gato/:id", upload.single("imagen"), actualizarGato);
+
 export default router;
